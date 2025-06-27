362a80ba17507cafe224b71b4c29bcb31e910b2b
Murfew <matthew.hurley953@gmail.com>
2025-06-27 15:29:06 -0400

    Implement GameHeader component and update App to render it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
+import GameHeader from "./GameHeader";
+
 function App() {
-  return;
+  return (
+    <div className="p-4">
+      <GameHeader />
+    </div>
+  )
 }
 
 export default App;
--- src/components/GameHeader.jsx
+++ src/components/GameHeader.jsx
@@ -1,5 +1,16 @@
-function GameHeader() {
-  return;
+function GameHeader({score, highScore}) {
+  return (
+    <div className="flex justify-between">
+      <div className="title">
+        <h1>Pokémon Memory Game</h1>
+        <p>Get points by clicking on an image, but don't click the same one more than once!</p>
+      </div>
+      <div className="score">
+        <p>Score: {score}</p>
+        <p>High Score: {highScore}</p>
+      </div>
+    </div>
+  )
 }
 
 export default GameHeader;
