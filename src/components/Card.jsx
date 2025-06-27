c5b6d22cbb8ee6f27ca230fb4399350a92470ec9
Murfew <matthew.hurley953@gmail.com>
2025-06-27 15:39:20 -0400

    Implement Card component to accept props and render image and name

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,10 @@
-function Card() {
-  return;
+function Card({imageURL, pkmnName}) {
+  return (
+    <div>
+      <img src={imageURL} alt={pkmnName} />
+      <p>{pkmnName}</p>
+    </div>
+  )
 }
 
 export default Card;
