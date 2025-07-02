a0192ae4dc8e55dc96531de1b7bbd9a4aa1a1df2
Murfew <matthew.hurley953@gmail.com>
2025-07-02 12:04:04 -0400

    Implement CardGrid component to render a list of Pokémon cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
+import CardGrid from "./CardGrid";
 import GameHeader from "./GameHeader";
+
+// TODO : Generate the random list of pokemon only the first time the app is loaded
+// Save this into an object to be passed to other components
 
 function App() {
   return (
     <div className="p-4">
       <GameHeader />
+      <CardGrid></CardGrid>
     </div>
   )
 }
--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,5 +1,13 @@
-function CardGrid() {
-  return;
+import Card from "./Card";
+
+function CardGrid({pkmn}) {
+  return (
+    <div>
+      {pkmn.map((item, index) => (
+        <Card key={index} imageURL={item.sprites.front_default} pkmnName={item.forms[0].name}></Card>
+      ))}
+    </div>
+  )
 }
 
 export default CardGrid;
