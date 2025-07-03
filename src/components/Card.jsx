9b6d27dd435b0d3c03f6cf376bba48923827c318
Murfew <matthew.hurley953@gmail.com>
2025-07-03 10:42:30 -0400

    Add styling to Card component and enhance Pokémon name display

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ function Card({imageURL, pkmnName, onClick}) {
 
 
   return (
-    <div onClick={onClick}>
+    <div className ="bg-[#2A2A40] border border-[#3C3C5A] rounded-2xl shadow-lg p-6 w-64 text-white hover:scale-105 transition-transform duration-200" onClick={onClick}>
       <img src={imageURL} alt={pkmnName} />
-      <p>{pkmnName.charAt(0).toUpperCase() + pkmnName.slice(1)}</p>
+      <p className="text-xl font-bold mb-2 text-[#FFD700]">{pkmnName.charAt(0).toUpperCase() + pkmnName.slice(1)}</p>
     </div>
   )
 }
